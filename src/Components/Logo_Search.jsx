36ae0baafc6594b0ca569ef45b4b4fe44fd3f2f7
sam <apple.jeet@gmail.com>
2024-08-27 21:34:47 +0530

    Style Changes

--- src/Components/Logo_Search.jsx
+++ src/Components/Logo_Search.jsx
@@ -1,8 +1,8 @@
 export default function Logo_Search({ setCity, city, weatherData }) {
   return (
-    <div className="flex flex-col gap-20 p-3">
+    <div className="flex flex-col p-3">
       <h1 className="text-xl">Weather app</h1>
-      <form className="self-center text-3xl flex gap-5 mb-6">
+      <form className="self-center text-3xl flex gap-5 mt-24">
         <label htmlFor="searchWeather" className="font-normal">
           Search
         </label>
@@ -15,6 +15,9 @@ export default function Logo_Search({ setCity, city, weatherData }) {
           onChange={(e) => setCity(e.target.value)}
         ></input>
       </form>
+      <span className="block text-center mb-6 text-xl">
+        Search by City or City, State
+      </span>
     </div>
   );
 }
